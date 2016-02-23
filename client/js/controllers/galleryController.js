74d05b519278b80a94c2d422da345823b9f2db04
jungyoseok <yasu3237@gmail.com>
2016-02-22 16:48:52 -0800

    fix gallery contd

--- client/js/controllers/galleryController.js
+++ client/js/controllers/galleryController.js
@@ -12,7 +12,7 @@ tgApp.controller('galleryController', ['$scope', '$routeParams', function($scope
       portfolio:[{
                   title: 'SALT',
                   desc: 'A publication celebrating the coastline of Wellington, New Zealand. This book follows the coastline around Wellington, with photographs and story.',
-                  url: '/gallery/courtneybragg/0/',
+                  url: '/gallery/courtneybragg/0#/',
                   imgs: [{src:'/creatives/courtneybragg/projects/SALT/1.jpg'},
                         {src:'/creatives/courtneybragg/projects/SALT/2.jpg'},
                         {src:'/creatives/courtneybragg/projects/SALT/3.jpg'},
@@ -27,7 +27,7 @@ tgApp.controller('galleryController', ['$scope', '$routeParams', function($scope
                 },{
                   title: 'DUSK & DAWN LOOKBOOK 2016',
                   desc: '@duskanddawnstore Photography - Evangeline Davis Hair/Make Up - Victoria Nunns Model - Kirvil @ Kirsty Bunny',
-                  url: '/gallery/courtneybragg/1/',
+                  url: '/gallery/courtneybragg/1#/',
                   imgs: [{src:'/creatives/courtneybragg/projects/DuskAndDawn/1.jpg'},
                         {src:'/creatives/courtneybragg/projects/DuskAndDawn/2.jpg'},
                         {src:'/creatives/courtneybragg/projects/DuskAndDawn/3.jpg'},
@@ -39,7 +39,7 @@ tgApp.controller('galleryController', ['$scope', '$routeParams', function($scope
                 },{
                   title: 'F*CK',
                   desc: 'A publication called F*CK, containing stories and theories of how the modern swear word "fuck" originated. The book was made digitally and by hand using letterpress and spray painting techniques.',
-                  url: '/gallery/courtneybragg/2/',
+                  url: '/gallery/courtneybragg/2#/',
                   imgs: [{src:'/creatives/courtneybragg/projects/F_CK/1.jpg'},
                         {src:'/creatives/courtneybragg/projects/F_CK/2.jpg'},
                         {src:'/creatives/courtneybragg/projects/F_CK/3.jpg'},
@@ -52,7 +52,7 @@ tgApp.controller('galleryController', ['$scope', '$routeParams', function($scope
                 },{
                   title: 'Death By Dagger',
                   desc: 'A book themed around the typographic symbol the dagger.',
-                  url: '/gallery/courtneybragg/3/',
+                  url: '/gallery/courtneybragg/3#/',
                   imgs: [{src:'/creatives/courtneybragg/projects/DeathByDagger/1.jpg'},
                         {src:'/creatives/courtneybragg/projects/DeathByDagger/2.jpg'},
                         {src:'/creatives/courtneybragg/projects/DeathByDagger/3.jpg'},
@@ -64,7 +64,7 @@ tgApp.controller('galleryController', ['$scope', '$routeParams', function($scope
                 },{
                   title: 'Ralph Hotere Black Light',
                   desc: 'Redesign of Ralph Hotere: Black Light the publication and art catalogue to accompany his exhibition.',
-                  url: '/gallery/courtneybragg/4/',
+                  url: '/gallery/courtneybragg/4#/',
                   imgs: [{src:'/creatives/courtneybragg/projects/RalphHotereBlackLight/1.jpg'},
                         {src:'/creatives/courtneybragg/projects/RalphHotereBlackLight/2.jpg'},
                         {src:'/creatives/courtneybragg/projects/RalphHotereBlackLight/3.jpg'},
@@ -72,7 +72,7 @@ tgApp.controller('galleryController', ['$scope', '$routeParams', function($scope
                 },{
                   title: 'The Māori New Year',
                   desc: 'A calendar celebrating the Māori new year, Matariki.',
-                  url: '/gallery/courtneybragg/5/',
+                  url: '/gallery/courtneybragg/5#/',
                   imgs: [{src:'/creatives/courtneybragg/projects/TheMaoriNewYear/1.jpg'}]
                 }
               ]},{
@@ -82,42 +82,42 @@ tgApp.controller('galleryController', ['$scope', '$routeParams', function($scope
       portfolio:[{
               title: 'Ariel',
               desc: 'Egestas adipiscing purus elementum risus turpis tincidunt',
-              url: '/gallery/daynamcc/0/',
+              url: '/gallery/daynamcc/0#/',
               imgs: [{src:'/creatives/daynamcc/projects/arielfinal.jpg'}]
             },{
               title: 'Audrey',
               desc: 'Egestas adipiscing purus elementum risus turpis tincidunt',
-              url: '/gallery/daynamcc/1/',
+              url: '/gallery/daynamcc/1#/',
               imgs: [{src:'/creatives/daynamcc/projects/audrey.jpg'}]
             },{
               title: 'Cara',
               desc: 'Egestas adipiscing purus elementum risus turpis tincidunt',
-              url: '/gallery/daynamcc/2/',
+              url: '/gallery/daynamcc/2#/',
               imgs: [{src:'/creatives/daynamcc/projects/cara.jpg'}]
             },{
               title: 'Cranberry',
               desc: 'Egestas adipiscing purus elementum risus turpis tincidunt',
-              url: '/gallery/daynamcc/3/',
+              url: '/gallery/daynamcc/3#/',
               imgs: [{src:'/creatives/daynamcc/projects/cranberry.jpg'}]
             },{
               title: 'Jade',
               desc: 'Egestas adipiscing purus elementum risus turpis tincidunt',
-              url: '/gallery/daynamcc/4/',
+              url: '/gallery/daynamcc/4#/',
               imgs: [{src:'/creatives/daynamcc/projects/jade.jpg'}]
             },{
               title: 'Mr. Carter',
               desc: 'Egestas adipiscing purus elementum risus turpis tincidunt',
-              url: '/gallery/daynamcc/5/',
+              url: '/gallery/daynamcc/5#/',
               imgs: [{src:'/creatives/daynamcc/projects/mrcarter.jpg'}]
             },{
               title: 'Symba',
               desc: 'Egestas adipiscing purus elementum risus turpis tincidunt',
-              url: '/gallery/daynamcc/6/',
+              url: '/gallery/daynamcc/6#/',
               imgs: [{src:'/creatives/daynamcc/projects/symba.jpg'}]
             },{
               title: 'Muhammad',
               desc: 'Egestas adipiscing purus elementum risus turpis tincidunt',
-              url: '/gallery/daynamcc/7/',
+              url: '/gallery/daynamcc/7#/',
               imgs: [{src:'/creatives/daynamcc/projects/muhammad.jpg'}]
             }]}
 
